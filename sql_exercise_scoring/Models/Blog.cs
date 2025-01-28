018ee45dbcb32a3f1e0cb868a7009b84ca060646
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2025-01-28 13:16:58 +0700

    ádasd

---
+++ sql_exercise_scoring/Models/Blog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sql_exercise_scoring.Models;
+
+public partial class Blog
+{
+    public int BlogId { get; set; }
+
+    public string BlogTitle { get; set; } = null!;
+
+    public string BlogContent { get; set; } = null!;
+
+    public DateTime BlogDate { get; set; }
+
+    public bool Published { get; set; }
+
+    public int CoderId { get; set; }
+
+    public bool PinHome { get; set; }
+
+    public virtual Coder Coder { get; set; } = null!;
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+}
---
+++ sql_exercise_scoring/Models/TestRun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sql_exercise_scoring.Models;
+
+public partial class TestRun
+{
+    public int TestRunId { get; set; }
+
+    public int SubmissionId { get; set; }
+
+    public int TestCaseId { get; set; }
+
+    public int TimeDuration { get; set; }
+
+    public int MemorySize { get; set; }
+
+    public string TestOutput { get; set; } = null!;
+
+    public string Result { get; set; } = null!;
+
+    public string? CheckerLog { get; set; }
+
+    public virtual Submission Submission { get; set; } = null!;
+
+    public virtual TestCase TestCase { get; set; } = null!;
+}
