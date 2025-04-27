92715902b8db5b445cb4537b83c0390867e8e216
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2025-04-27 07:14:57 +0700

    ádasd

--- ProCoder/ProCoder/Models/ViewModels/HomeViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProCoder.Models.ViewModels
-{
-    public class HomeViewModel
-    {
-        public List<Blog> PinnedBlogs { get; set; }
-        public List<TopCoderViewModel> TopCoders { get; set; }
-        public List<Problem> RecentProblems { get; set; }
-    }
-} 
